047e7c6d509a1e617b02d423e15ccbb1c118ff40
RLOG89 <ross.loggie@gmail.com>
2017-01-31 14:39:39 +0000

    test hash contains raw and parsed date

--- models/specs/dates_spec.rb
+++ models/specs/dates_spec.rb
@@ -10,13 +10,17 @@ class TestDates < MiniTest::Test
   end
 
   def test_can_get_dates
-    element_0 = @input.get_dates.at(0)
     refute_nil([], @input.get_dates)
   end
 
-  def test_can_parse_date
-    element_0 = @input.get_dates.at(0)
-    assert_equal('27/01/2017', @input.parse_date(element_0))
+  def test_key_is_raw_date
+    assert_equal(true, @input.get_dates.has_key?('2017-01-27'))
+    assert_equal(false, @input.get_dates.has_key?('27/01/2017'))
+  end
+
+  def test_value_is_parsed_date
+    assert_equal(true, @input.get_dates.has_value?('27/01/2017'))
+    assert_equal(false, @input.get_dates.has_value?('2017-01-27'))
   end
 
 end
